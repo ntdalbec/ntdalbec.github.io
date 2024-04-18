d924d0f3ed0a0350bd21d1e6e2566d1edad8cf52
ntdalbec <32275348+ntdalbec@users.noreply.github.com>
2024-04-18 19:58:37 -0400

    customize metadata and remove reminder messagebox

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,13 @@
+const BASE_URL = "https://ntdalbec.github.io";
+
 module.exports = {
-	title: "Eleventy Base Blog v8",
-	url: "https://example.com/",
+	title: "〖ntdalbec〗",
+	url: BASE_URL,
 	language: "en",
-	description: "I am writing about my experiences as a naval navel-gazer.",
+	description: "A collection of works from your friendly neighborhood techno-hillbilly.",
 	author: {
-		name: "Your Name Here",
-		email: "youremailaddress@example.com",
-		url: "https://example.com/about-me/"
+		name: "Nathan Dalbec",
+		email: "ntdalbec@gmail.com",
+		url: BASE_URL + "/about/"
 	}
 }
